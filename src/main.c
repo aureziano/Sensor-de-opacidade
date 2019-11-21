1c7ceb627229d322d2632d7947041c620c69178b
aureziano <aureziano@hotmail.com>
2019-11-21 17:31:00 -0300

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -12,6 +12,8 @@ int j; //contador nao opaco
 #define PORTALED	GPIOC
 
 #define SENSOR		GPIO_Pin_0 // sensor de opacidade
+#define OBJ		GPIO_Pin_1 // passagem de objeto
+#define ESTEIRA		GPIO_Pin_2 // passagem de objeto
 #define PORTABOTAO	GPIOA
 
 
@@ -26,20 +28,33 @@ int main()
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(PORTALED, &GPIO_InitStructure);
 
+	GPIO_InitStructure.GPIO_Pin = ESTEIRA ; //PIN 2 PORT A - ligar um relé
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(PORTABOTAO, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = OBJ;//PIN 1 PORT A - objeto entrou na esteira
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;// Inserção flutuante
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(PORTABOTAO, &GPIO_InitStructure);
+
 	GPIO_InitStructure.GPIO_Pin = SENSOR;//PIN 0 configurado como entrada(nesse caso botão no exemplo) na GPIO Port A
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;// Inserção flutuante
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(PORTABOTAO, &GPIO_InitStructure);
 
 	TIM_TimeBaseInitTypeDef TimerInitStructure;
-	TimerInitStructure.TIM_Prescaler = 4000-1; //Prescaler 72MHz/4000 = 18Khz => 18000 ciclos/sec
-	TimerInitStructure.TIM_Period = 4000-1;
-	TimerInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
-	TimerInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-	TimerInitStructure.TIM_RepetitionCounter = 0;
+	//Configura o divisor do timer para 1000 ciclos por segundo.
+	//O clock do sistema roda a 72Mhz, logo:
+	//72000000 / 720000 = 1000 ciclos/seg
+	TimerInitStructure.TIM_Prescaler = SystemCoreClock/100 -1;
+	TimerInitStructure.TIM_Period = 4000-1;//Contamos até 1000 (0..999), assim temos 1000 contagens por segundo. Com isso teremos um overflow por segundo.
+	TimerInitStructure.TIM_CounterMode = TIM_CounterMode_Up;//contador conta de 0 até 999, de forma crescente
+	TimerInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;//Não queremos nenhum divisor de clock, o calculo é com os 72Mhz.
+	TimerInitStructure.TIM_RepetitionCounter = 0;//Zerando tudo.
 	TIM_TimeBaseInit(TIM2, &TimerInitStructure);
-	TIM_Cmd(TIM2, ENABLE);
-	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+	TIM_Cmd(TIM2, ENABLE);//Ligando o timer
+	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);//Habilitando a atualização do bit de interrupção para o overflow.
 
 	 lcd_init(); //iniciação do LCD
 	 KeypadInit(); //Opção de utilização de um teclado matricial 4x3
@@ -51,17 +66,11 @@ int main()
 
 	 while(1)
 	 {
-		 GPIO_SetBits(PORTALED,LEDAZUL); // liga esteira
-		 GPIO_ResetBits(PORTALED,LEDVERDE); // apaga led verde
-		 GPIO_ResetBits(PORTABOTAO,SENSOR); // seta 0 para o sensor
-		 lcd_clear();
-		 lcd_line1(); //habilita linha 1 do LCD
-//		  char result;
-//		  if (i >= 10) {
-//		      result = 'A' + i - 10;
-//		  } else {
-//		      result = '0' + i;
-//		  }
+		  GPIO_SetBits(PORTALED,LEDAZUL); // liga esteira
+		  GPIO_ResetBits(PORTALED,LEDVERDE); // apaga led verde
+		  GPIO_ResetBits(PORTABOTAO,SENSOR); // seta 0 para o sensor
+		  lcd_clear();
+		  lcd_line1(); //habilita linha 1 do LCD
 		  itoa(i,result,10); // função que transforma int em string
 		  lcd_print("CONTADOR:"); //printa na linha 1
 		  lcd_line2(); //habilita linha 2
@@ -76,15 +85,10 @@ int main()
 
 					GPIO_SetBits(PORTALED,LEDVERDE);
 					i = i + 1;
-//					if (i >= 10) {
-//						result = 'A' + i - 10;
-//					} else {
-//						result = '0' + i;
-//					}
 					itoa(i,result,10);
 					lcd_line2();
 					lcd_print(result);
-					delay_ms(1000);
+//					delay_ms(1000);
 					//Como realizamos o trabalho necessário, evitamos que esta interrupção siga sendo avaliada por demais partes do CMSIS.
 				    //Para isso só precisamos limpar o BIT de interrupção pendente de overflow.
 					TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
@@ -93,19 +97,20 @@ int main()
 		  }
 		  else
 		  {
-			  	  j = j + 1;
-//			  	  if (j >= 10) {
-//					result = 'A' + j - 10;
-//				  } else {
-//					result = '0' + j;
-//				  }
-			  	  itoa(j,result,10);
+			  if (TIM_GetITStatus(TIM2, TIM_IT_Update))
+			  {
+				  j = j + 1;
+				  itoa(j,result,10);
 				  lcd_clear();
 				  lcd_line1();
 				  lcd_print("NAO OPACO:");
 				  lcd_line2();
 				  lcd_print(result);
-				  delay_ms(3000);
+//				  delay_ms(3000);
+				  TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
+
+			  }
+
 
 		  }
 
