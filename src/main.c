76d11494878b94dc5b3ca3005b0f07601bf2a1b4
aureziano <aureziano@hotmail.com>
2019-11-21 23:29:44 -0300

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -7,23 +7,24 @@
 
 int i; //contador opaco
 int j; //contador nao opaco
+int total;
 #define LEDVERDE	GPIO_Pin_9 //Esteira
 #define LEDAZUL		GPIO_Pin_8 //Aviso de nao opaco
 #define PORTALED	GPIOC
 
 #define SENSOR		GPIO_Pin_0 // sensor de opacidade
-#define OBJ		GPIO_Pin_1 //Aviso de nao opaco
-#define ESTEIRA		GPIO_Pin_2 //Aviso de nao opaco
+#define OBJ		GPIO_Pin_3 //Aviso de nao opaco
+#define ESTEIRA		GPIO_Pin_4 //Aviso de nao opaco
 #define PORTABOTAO	GPIOA
 
 
 int main()
 {
-	GPIO_InitTypeDef GPIO_InitStructure;//inicia configuração das GPIO
+	GPIO_InitTypeDef GPIO_InitStructure;//inicia configuracao das GPIO
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC,  ENABLE);//ativa clock para GPIO Port C
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);//Ativa o clock para a interrupção TIM2
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);//Ativa o clock para a interrupcao TIM2
 
-	GPIO_InitStructure.GPIO_Pin = LEDAZUL | LEDVERDE ; //Configuração do Pino 8 e 9 para saída
+	GPIO_InitStructure.GPIO_Pin = LEDAZUL | LEDVERDE ; //Configuracao do Pino 8 e 9 para saida
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(PORTALED, &GPIO_InitStructure);
@@ -33,81 +34,119 @@ int main()
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(PORTABOTAO, &GPIO_InitStructure);
 
-	GPIO_InitStructure.GPIO_Pin = SENSOR;//PIN 0 configurado como entrada(nesse caso botão no exemplo) na GPIO Port A
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;// Inserção flutuante
+	GPIO_InitStructure.GPIO_Pin = SENSOR;//PIN 0 configurado como entrada(nesse caso botao no exemplo) na GPIO Port A
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;// Insercao flutuante
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(PORTABOTAO, &GPIO_InitStructure);
 
 	GPIO_InitStructure.GPIO_Pin = OBJ;//PIN 1 configurado como entrada(nesse caso botao no exemplo) na GPIO Port A
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;// Inserção flutuante
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;// Inserccao flutuante
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(PORTABOTAO, &GPIO_InitStructure);
 
 	TIM_TimeBaseInitTypeDef TimerInitStructure;
-	TimerInitStructure.TIM_Prescaler = 4000-1; //Prescaler 72MHz/4000 = 18Khz => 18000 ciclos/sec
-	TimerInitStructure.TIM_Period = 4000-1;
-	TimerInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
-	TimerInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-	TimerInitStructure.TIM_RepetitionCounter = 0;
+	//Configura o divisor do timer para 1000 ciclos por segundo.
+	//O clock do sistema roda a 72Mhz, logo:
+	//72000000 / 720000 = 1000 ciclos/seg
+	TimerInitStructure.TIM_Prescaler = SystemCoreClock/100 -1;
+	TimerInitStructure.TIM_Period = 4000-1;//Contamos at� 1000 (0..999), assim temos 1000 contagens por segundo. Com isso teremos um overflow por segundo.
+	TimerInitStructure.TIM_CounterMode = TIM_CounterMode_Up;//contador conta de 0 at� 999, de forma crescente
+	TimerInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;//N�o queremos nenhum divisor de clock, o calculo � com os 72Mhz.
+	TimerInitStructure.TIM_RepetitionCounter = 0;//Zerando tudo.
 	TIM_TimeBaseInit(TIM2, &TimerInitStructure);
-	TIM_Cmd(TIM2, ENABLE);
-	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+	TIM_Cmd(TIM2, ENABLE);//Ligando o timer
+	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);//Habilitando a atualiza�ao do bit de interrup�ao para o overflow.
 
-	 lcd_init(); //iniciação do LCD
-	 KeypadInit(); //Opção de utilização de um teclado matricial 4x3
-	 delay_init();// iniciação da função dalay
+	 lcd_init(); //iniciacao do LCD
+	 KeypadInit(); //Opcao de utilizacao de um teclado matricial 4x3
+	 delay_init();// iniciacao da funcao dalay
 	 lcd_clear(); //Limpa LCD
 	 i=0; //contador de opacidade zerado
 	 j=0; // contador de nao apacidade zerado
+	 total = 0;
 	 char result[20];
 
 	 while(1)
 	 {
-		 GPIO_SetBits(PORTALED,LEDAZUL); // liga esteira
-		 GPIO_SetBits(PORTABOTAO,ESTEIRA); // liga esteira
-		 GPIO_ResetBits(PORTALED,LEDVERDE); // apaga led verde
-		 GPIO_ResetBits(PORTABOTAO,SENSOR); // seta 0 para o sensor
-		 lcd_clear();
-		 lcd_line1(); //habilita linha 1 do LCD
-		  itoa(i,result,10); // função que transforma int em string
-		  lcd_print("CONTADOR:"); //printa na linha 1
-		  lcd_line2(); //habilita linha 2
-		  lcd_print(result); // printa na linha 2 o contador de opaco
-		  delay_ms(50); // dalay de 50 ms
+		 while(total < 20)
+		 {
+			 GPIO_SetBits(PORTALED,LEDAZUL); // liga esteira
+			 GPIO_ResetBits(PORTALED,LEDVERDE); // apaga led verde
+			 GPIO_ResetBits(PORTABOTAO,SENSOR); // seta 0 para o sensor
+			 GPIO_SetBits(PORTABOTAO,ESTEIRA); // liga esteira
+			 lcd_clear();
+			 total = i +j;
+			 if (GPIO_ReadInputDataBit(PORTABOTAO,OBJ) == 1) //se o sensor encontra elemento nao opaco
+			 {
+				 GPIO_SetBits(PORTALED,LEDVERDE); // liga esteira
+				 lcd_clear();
+				 lcd_line1(); //habilita linha 1 do LCD
+				 lcd_print("OBJ NA ESTEIRA"); //printa na linha 1
+				 delay_ms(1000);
+				 itoa(total,result,10); // funcao que transforma int em string
+				 lcd_clear();
+				 lcd_line1(); //habilita linha 1 do LCD
+				 lcd_print("TOTAL:"); //printa na linha 1
+				 lcd_line2(); //habilita linha 2
+				 lcd_print(result); // printa na linha 2 o contador de opaco
+				 delay_ms(1000); // dalay de 50 ms
 
-		  if (GPIO_ReadInputDataBit(PORTABOTAO,SENSOR)) //se o sensor encontra elemento não opaco
-		  {
+				  if (GPIO_ReadInputDataBit(PORTABOTAO,SENSOR) == 1) //se o sensor encontra elemento nao opaco
+				  {
 
-			  GPIO_ResetBits(PORTALED,LEDAZUL); //seta 0 no LED azul
-			  if (TIM_GetITStatus(TIM2, TIM_IT_Update)) { //aciona o temporizador TIM2
+					  GPIO_ResetBits(PORTALED,LEDAZUL); //seta 0 no LED azul
+					  GPIO_SetBits(PORTALED,LEDVERDE);
+					  i = i + 1;
+					  itoa(i,result,10);
+					  if (TIM_GetITStatus(TIM2, TIM_IT_Update)) //aciona o temporizador TIM2
+					  {
+						  lcd_clear();
+						  lcd_line1(); //habilita linha 1 do LCD
+						  lcd_print("NAO OPACO:"); //printa na linha 1
+						  lcd_line2();
+						  lcd_print(result);
+						  delay_ms(1000);
+						  //Como realizamos o trabalho necessário, evitamos que esta interrupcao siga sendo avaliada por demais partes do CMSIS.
+						  //Para isso precisamos limpar o BIT de interrupcao pendente de overflow.
+						  TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
+						}
 
-					GPIO_SetBits(PORTALED,LEDVERDE);
-					i = i + 1;
-					itoa(i,result,10);
-					lcd_line2();
-					lcd_print(result);
-					delay_ms(1000);
-					//Como realizamos o trabalho necessário, evitamos que esta interrupção siga sendo avaliada por demais partes do CMSIS.
-				    //Para isso só precisamos limpar o BIT de interrupção pendente de overflow.
-					TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
-				}
+				  }
+				  else
+				  {
+						  j = j + 1;
+						  itoa(j,result,10);
+						  if (TIM_GetITStatus(TIM2, TIM_IT_Update)) //aciona o temporizador TIM2
+						  {
+							  lcd_clear();
+							  lcd_line1();
+							  lcd_print("OPACO:");
+							  lcd_line2();
+							  lcd_print(result);
+							  delay_ms(3000);
+							  TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
+						  }
 
-		  }
-		  else
-		  {
-			  	  j = j + 1;
-			  	  itoa(j,result,10);
-				  lcd_clear();
-				  lcd_line1();
-				  lcd_print("NAO OPACO:");
-				  lcd_line2();
-				  lcd_print(result);
-				  delay_ms(3000);
+				  }
+			 }
+			 else
+			 {
+				 if (TIM_GetITStatus(TIM2, TIM_IT_Update)) //aciona o temporizador TIM2
+				 {
+					 GPIO_ResetBits(PORTALED,LEDVERDE); // liga esteira
+					 lcd_clear();
+					 lcd_line1(); //habilita linha 1 do LCD
+					 lcd_print("ESTEIRA VAZIA"); //printa na linha 1
+					 delay_ms(3000);
+					 TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
 
-		  }
-
-
+				 }
+			 }
+		 }
+		 GPIO_ResetBits(PORTABOTAO,ESTEIRA); // desliga esteira apos 20 itens
 	 }
+
+
 }
 
 
